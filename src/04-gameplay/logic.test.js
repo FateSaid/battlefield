f3d8148acbfd83d79844310083700497aa30cf65
Fatesaid <fatesaid56@gmail.com>
2024-11-25 17:04:14 +0100

    add test to check winner

--- src/04-gameplay/logic.test.js
+++ src/04-gameplay/logic.test.js
@@ -28,3 +28,52 @@ describe("Checking GameController players", () => {
     expect(gameplay.getActivePlayer().game.missedAttacks).toEqual(shouldHave);
   });
 });
+
+describe("Calculate Winners", () => {
+  let gameplay = GameController("User", "Computer");
+
+  test("Should say winner when all ships sunk", () => {
+    /* player 1 
+    [0, 0], [0, 4]
+    [5, 5], [5, 8]
+    [4, 3], [4, 5]
+    [8, 7], [8, 9]
+    [2, 4], [2, 5]
+
+    */
+
+    gameplay.playRound([0, 0]);
+    gameplay.playRound([0, 0]);
+    gameplay.playRound([0, 1]);
+    gameplay.playRound([0, 1]);
+    gameplay.playRound([0, 2]);
+    gameplay.playRound([0, 2]);
+    gameplay.playRound([0, 3]);
+    gameplay.playRound([0, 3]);
+    gameplay.playRound([0, 4]);
+    gameplay.playRound([0, 4]);
+    gameplay.playRound([5, 5]);
+    gameplay.playRound([5, 5]);
+    gameplay.playRound([5, 6]);
+    gameplay.playRound([5, 6]);
+    gameplay.playRound([5, 7]);
+    gameplay.playRound([5, 7]);
+    gameplay.playRound([5, 8]);
+    gameplay.playRound([5, 8]);
+    gameplay.playRound([4, 3]);
+    gameplay.playRound([4, 3]);
+    gameplay.playRound([4, 4]);
+    gameplay.playRound([4, 5]);
+    gameplay.playRound([4, 5]);
+    gameplay.playRound([8, 7]);
+    gameplay.playRound([8, 7]);
+    gameplay.playRound([8, 8]);
+    gameplay.playRound([8, 8]);
+    gameplay.playRound([8, 9]);
+    gameplay.playRound([8, 9]);
+    gameplay.playRound([2, 4]);
+    gameplay.playRound([2, 4]);
+    gameplay.playRound([2, 5]);
+    expect(gameplay.playRound([2, 5])).toBe("Computer is Winner");
+  });
+});
